54d5ea5083be7c6868759e6b1b076b479e8be0b3
dominicpd <dominic.rafer@peakdesign.com>
2025-07-23 08:31:56 +0800

    fix: textarea label

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -11,7 +11,7 @@ const Textarea = ({
   return (
     <div className="relative">
       <textarea
-        id="floating_textarea"
+        id={label}
         rows={6}
         className={clsx(
           "block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-400 appearance-none dark:text-white dark:focus:border-accent focus:outline-none focus:ring-0 focus:border-accent peer transition-all duration-300 resize-none",
@@ -21,7 +21,7 @@ const Textarea = ({
         {...properties}
       />
       <label
-        htmlFor="floating_textarea"
+        htmlFor={label}
         className="absolute text-sm text-gray-300 cursor-text duration-300 transform -translate-y-4 scale-90 top-2 z-10 origin-[0] bg-secondary px-2 peer-focus:px-2 peer-focus:text-accent peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-6 peer-focus:top-2 peer-focus:scale-85 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
       >
         {label}
