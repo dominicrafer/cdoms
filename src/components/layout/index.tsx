1a09ec8413ad7013a0ad2aaedaf281521c8457d6
Dominic Rafer <raferdominic@gmail.com>
2025-07-24 17:11:02 +0800

    fix: apply bg color to body instead of layout div

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -4,13 +4,11 @@ import SpotlightCursor from "./spotlight";
 import FloatingContactForm from "./floating-contact-form";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className=" bg-[#131924]">
-      <div className="sm:max-w-screen-md lg:flex-row lg:gap-4 md:max-w-screen-xl flex flex-col justify-center items-center lg:items-start lg:mx-auto">
-        <SpotlightCursor />
-        <Aside />
-        <Content>{children}</Content>
-        <FloatingContactForm></FloatingContactForm>
-      </div>
+    <div className="sm:max-w-screen-md lg:flex-row lg:gap-4 md:max-w-screen-xl flex flex-col justify-center items-center lg:items-start lg:mx-auto">
+      <SpotlightCursor />
+      <Aside />
+      <Content>{children}</Content>
+      <FloatingContactForm></FloatingContactForm>
     </div>
   );
 };
